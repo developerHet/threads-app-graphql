f508a74cc194a6b9b520a5954d895deea95fc534
hetdrisk <het.patel@drisk.io>
2023-12-16 22:25:33 +0530

    Code Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
-import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
-import { prismaClient } from "./lib/db";
+import createApolloGraphqlServer from "./graphql";
 
 async function init() {
   const app = express();
@@ -10,59 +9,11 @@ async function init() {
 
   const PORT = Number(process.env.PORT) || 8000;
 
-  // create graphql server
-  const gqlserver = new ApolloServer({
-    typeDefs: `
-        type Query {
-            hello: String,
-            say(name: String): String
-        }
-        type Mutation {
-          createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
-        }
-    `,
-    resolvers: {
-      Query: {
-        hello: () => "Hello form grapql server",
-        say: (_, { name }: { name: String }) => `Hello from ${name}`,
-      },
-      Mutation: {
-        createUser: async (
-          _,
-          {
-            firstName,
-            lastName,
-            email,
-            password,
-          }: {
-            firstName: string;
-            lastName: string;
-            email: string;
-            password: string;
-          }
-        ) => {
-          await prismaClient.user.create({
-            data: {
-              email,
-              firstName,
-              lastName,
-              password,
-              salt: "random_salt",
-            },
-          });
-          return true;
-        },
-      },
-    },
-  });
-
-  await gqlserver.start();
-
   app.get("/", (req, res) => {
     res.json({ message: "Server is up and running" });
   });
 
-  app.use("/graphql", expressMiddleware(gqlserver));
+  app.use("/graphql", expressMiddleware(await createApolloGraphqlServer()));
 
   app.listen(PORT, () => {
     console.log(`Server is started at PORT: ${PORT}`);
